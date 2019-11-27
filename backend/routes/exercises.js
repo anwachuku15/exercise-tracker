2008bfbf748a3e15970984661c31e141ac6d6009
anwachuku15 <anwachuku15@gmail.com>
2019-11-27 15:26:48 -0500

    complete

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,13 +1,9 @@
 const router = require('express').Router();
 let Exercise = require('../models/exercise.model');
-let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
     Exercise.find()
         .then(exercises => res.json(exercises))
-        .catch(err => res.status(400).json('Error: ' + err));
-    User.find()
-        .then(users => res.json(users))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
